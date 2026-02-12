134975e8e809c5cf9b6a744ce58e42322f6f4bae
Aparkin <aparkin2017@mail.ru>
2026-02-13 00:36:47 +0400

    first commit

---
+++ src/main/java/ru/ssau/tk/faible/coplate/coplatebackend/CoplateBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.faible.coplate.coplatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoplateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoplateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ssau/tk/faible/coplate/coplatebackend/CoplateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.faible.coplate.coplatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoplateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
